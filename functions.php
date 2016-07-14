370810e687f0aba0e9acc3fa695707b68cb4b3f2
AndreaTurco <sigturk@gmail.com>
2016-07-14 08:37:58 +0200

    check token

--- functions.php
+++ functions.php
@@ -14,4 +14,24 @@ function generateFormToken($form) {
 
 }
 
+function verifyFormToken($form) {
+
+    // check if a session is started and a token is transmitted, if not return an error
+    if(!isset($_SESSION[$form.'_token'])) {
+        return false;
+    }
+
+    // check if the form is sent with token in it
+    if(!isset($_POST['token'])) {
+        return false;
+    }
+
+    // compare the tokens against each other if they are still the same
+    if ($_SESSION[$form.'_token'] !== $_POST['token']) {
+        return false;
+    }
+
+    return true;
+}
+
 ?>
